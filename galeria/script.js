c72a71a8983c23d400768b9aed9231d185874b8a
juaniike <ignaciojuan975@gmail.com>
2025-05-06 11:24:36 -0300

    Galeria-Agregada

--- galeria/script.js
+++ galeria/script.js
@@ -1,13 +1,25 @@
-const imagenTamaño = document.querySelectorAll(".foto")
+document.addEventListener("DOMContentLoaded", () => {
+  const imagenTamaño = document.querySelectorAll(".foto");
 
-
-imagenTamaño.forEach((imagen) => {
+  imagenTamaño.forEach((imagen) => {
     imagen.addEventListener("click", () => {
+      const imagenCentrada = imagen.style.position === "fixed";
+
+      if (imagenCentrada) {
+        imagen.style.position = "";
+        imagen.style.top = "";
+        imagen.style.left = "";
+        imagen.style.transform = "";
+        imagen.style.zIndex = "";
+        imagen.style.width = "";
+      } else {
         imagen.style.position = "fixed";
         imagen.style.top = "50%";
         imagen.style.left = "50%";
         imagen.style.transform = "translate(-50%, -50%)";
         imagen.style.zIndex = "999";
         imagen.style.width = "45vw";
-    })
-})
+      }
+    });
+  });
+});
