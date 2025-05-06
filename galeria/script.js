1856993909d8bb5ca5ea5288072c810fb9bb12a6
juaniike <ignaciojuan975@gmail.com>
2025-05-06 16:40:45 -0300

    Falta-Unificar

--- galeria/script.js
+++ galeria/script.js
@@ -1,24 +1,75 @@
 document.addEventListener("DOMContentLoaded", () => {
   const imagenTamaño = document.querySelectorAll(".foto");
+  const overlay = document.getElementById("overlay");
 
   imagenTamaño.forEach((imagen) => {
     imagen.addEventListener("click", () => {
-      const imagenCentrada = imagen.style.position === "fixed";
+      const imagenCentrada = imagen.classList.contains("imagenCentrada");
 
       if (imagenCentrada) {
-        imagen.style.position = "";
-        imagen.style.top = "";
-        imagen.style.left = "";
-        imagen.style.transform = "";
-        imagen.style.zIndex = "";
-        imagen.style.width = "";
+        imagen.classList.remove("imagenCentrada");
+        overlay.classList.add("overlayGaleria");
       } else {
-        imagen.style.position = "fixed";
-        imagen.style.top = "50%";
-        imagen.style.left = "50%";
-        imagen.style.transform = "translate(-50%, -50%)";
-        imagen.style.zIndex = "999";
-        imagen.style.width = "45vw";
+        document.querySelectorAll(".foto.imagenCentrada").forEach((imagen) => {
+          imagen.classList.remove("imagenCentrada");
+        });
+        imagen.classList.add("imagenCentrada");
+        overlay.classList.remove("overlayGaleria");
+      }
+    });
+  });
+
+  overlay.addEventListener("click", (e) => {
+    if (
+      !e.target.closest(".fotoContenedor") &&
+      !e.target.closest(".btnIzq") &&
+      !e.target.closest(".btnDer")
+    ) {
+      document
+        .querySelector(".foto.imagenCentrada")
+        ?.classList.remove("imagenCentrada");
+      overlay.classList.add("overlayGaleria");
+    }
+  });
+
+  ////////////////////////////////////////////////////////////////
+
+  const imagen = document.querySelector(".galeriaFotos");
+  const imagenes = [
+    "images/Spinetta.jpg",
+    "images/Goldin.jpg",
+    "images/Calamaro.jpg",
+    "images/Cerati.jpg",
+    "images/Fito.jpg",
+    "images/Mateos.jpg",
+  ];
+
+  let i = 0;
+
+  function previousChange() {
+    i--;
+    if (i < 0) {
+      i = imagenes.length - 1; // vuelve al final
+    }
+    imagen.setAttribute("src", imagenes[i]);
+  }
+
+  function nextChange() {
+    i++;
+    if (i >= imagenes.length) {
+      i = 0; // vuelve al principio
+    }
+    imagen.setAttribute("src", imagenes[i]);
+  }
+
+  const botones = document.querySelectorAll("button");
+
+  botones.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      if (btn.classList.contains("btnIzq")) {
+        previousChange();
+      } else {
+        nextChange();
       }
     });
   });
